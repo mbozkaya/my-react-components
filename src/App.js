91ce384ef89667be74b689314d4d1e60543bdc69
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-06-28 15:30:34 +0300

    image upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import CountDown from './Components/CountDown/CountDown';
 
 function App() {
-  const begin = new Date(2020, 5, 27, 20, 0);
+  const begin = new Date(2020, 5, 28, 14, 35);
   return (
     <>
       <CountDown
         begin={begin}
         onTimeEnd ={()=>alert('The time was end')}
+        dangerZoneMinute={25}
+        maxMinute={35}
       />
     </>
   );
---
+++ src/Components/CountDown/readme.md
@@ -0,0 +1,11 @@
+# Time Counter
+
+Style Reference : [CSSTricks](https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/)
+
+image-1
+
+![][https://github.com/mbozkaya/MyReactComponents/tree/master/src/Components/count1.jpg]
+
+image-2
+
+![][https://github.com/mbozkaya/MyReactComponents/tree/master/src/Components/count2.jpg]
